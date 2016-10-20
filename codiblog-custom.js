a7e02be2a519b33d6bcc478c7ae9621a6ac86087
MD Jillur Rahman <modina00mobzoo@gmail.com>
2016-10-20 08:51:08 +0600

    Create codiblog-custom.js

---
+++ codiblog-custom.js
@@ -0,0 +1,11 @@
+$(&#39;.sidebar .tune&#39;).attr(&#39;class&#39;, function(i) {
+				return &#39;tune color-&#39; + i;
+			});
+$(function(){
+			  $(&#39;#comment-holder a[rel$=nofollow]&#39;).attr(&quot;target&quot;,&quot;_blank&quot;);
+			});
+(function() {
+			var po = document.createElement(&#39;script&#39;); po.type = &#39;text/javascript&#39;; po.async = true;
+			po.src = &#39;https://apis.google.com/js/plusone.js&#39;;
+			var s = document.getElementsByTagName(&#39;script&#39;)[0]; s.parentNode.insertBefore(po, s);
+})();
